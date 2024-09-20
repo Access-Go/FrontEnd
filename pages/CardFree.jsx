8433d384003b80901b1d0f316da5b74aacdc4829
Raul Elizalde <raulei2323@gmail.com>
2024-09-20 01:02:35 -0600

    commit1

---
+++ pages/CardFree.jsx
@@ -0,0 +1,56 @@
+import Layout from '@/components/Layout';
+
+export default function CardFree() {
+  return (
+    <Layout>
+      <div className='grid grid-row: auto '>
+        <div className='w-[118px] h-[62px] flex justify-center items-center'>
+          <p className='text-[#2F4F4F] text-sm'>¡AccessGo!</p>
+        </div>
+        <img className='w-[236px] h-[300px]' src='' alt='Lugar accesible' />
+
+        <section className='w-[328px] h-[418px] grid grid-row: auto'>
+          <p className='w-[189px] h-40px] text-[#7E952A]'>Cielito querido</p>
+          <img className='w-[150px] h-[30px]' src='' alt='' />
+          <p className='w-[328px] h-[80px]'>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laboriosam
+            deleniti maiores ullam aut esse, alias, adipisci quasi voluptates
+            saepe quaerat maxime tempore facilis porro voluptatibus et nostrum
+            doloribus dolorum repellat.
+          </p>
+          <p>Horarios</p>
+
+          <img src='' alt='Schedule' />
+          <p>D, L, Ma, Mi, J, V, S</p>
+          <img src='' alt='Clock' />
+          <p>01:00PM</p>
+          <img src='' alt='Clock' />
+          <p>12:00AM</p>
+          <select name='' id=''>
+            <option value='Place'>Juarez 6600 Ciudad de Mexico</option>
+            <div>
+              <img src='' alt='motorDisability' />
+              <img src='' alt='blindHelp' />
+              <img src='' alt='deafHelp' />
+              <img src='' alt='neurodivergence' />
+            </div>
+          </select>
+        </section>
+        <section>
+          <a href=''>Dejar un comentario</a>
+          <div>
+            <img src='' alt='profilePicture' />
+            <p>Jhon Doe</p>
+            <img src='' alt='stars' />
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis
+              assumenda consequuntur sint exercitationem ipsam, inventore
+              obcaecati voluptas nulla cupiditate alias tempora eligendi sequi
+              accusantium, ad sed odio saepe natus consectetur.
+            </p>
+          </div>
+        </section>
+      </div>
+    </Layout>
+  );
+}
